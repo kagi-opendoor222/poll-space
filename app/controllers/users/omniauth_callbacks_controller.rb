8a4416f20cdc53d812aa5b98e454bc08c4ead9d5
kagi-opendoor222 <38743710+kagi-opendoor222@users.noreply.github.com>
2019-03-22 15:45:15 +0900

    edit OmniauthCallbacksController to sign_in from omniauth

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,2 +1,13 @@
 class Users::OmniauthCallbacksController < ApplicationController
+  def twitter
+    @user = User.from_omniauth(request.env["omniauth.auth"])
+    if @user.persisted?
+      sign_in_and_redirect @user, event: :authentication #this will throw if @user is not activate
+      flash[:notice] = "success!"
+       binding.pry
+    else
+      session["devise.twitter_data"] = request.env["omniauth.auth"]
+      redirect_to new_user_registration_url
+    end
+  end
 end
