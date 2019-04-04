4bada1765a9f9f17847c810dc37f606d5146b3dc
kagi-opendoor222 <38743710+kagi-opendoor222@users.noreply.github.com>
2019-04-04 14:58:17 +0900

    define to_ratio to translate number of votes to vote_ratio

--- app/models/group.rb
+++ app/models/group.rb
@@ -8,6 +8,12 @@ class Group < ApplicationRecord
   def self.votes_counts
     self.all.map(&:votes_count)
   end
+  def self.to_ratio(counts)
+    return counts.map do |num|
+      (num / counts.sum.to_f * 100).round
+    end
+  end
+
   def vote_ratio
     all_counts = self.theme.groups.votes_counts
     return (self.votes.length.to_f / all_counts.sum * 100).round
