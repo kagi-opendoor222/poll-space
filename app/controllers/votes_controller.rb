c6c7577081c7807f5e0185d2ecd7995d7a30cbe6
kagi-opendoor222 <38743710+kagi-opendoor222@users.noreply.github.com>
2019-08-26 16:55:51 +0900

    tmp

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,5 +3,6 @@ class VotesController < ApplicationController
   def create
     group = Group.find(params[:group_id])
     Vote.create(group_id: params[:group_id], user_id: current_user.id)
+    redirect_to theme_path(group.theme.id)
   end
 end
