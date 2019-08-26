8d68f84326ab57af18646581ecc20e503217bb7e
kagi-opendoor222 <38743710+kagi-opendoor222@users.noreply.github.com>
2019-08-26 17:09:28 +0900

    tmp

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -1,5 +1,5 @@
 class VotesController < ApplicationController
-  before_action :authenticate_user!, only: [:create]
+  # before_action :authenticate_user!, only: [:create]
   def create
     group = Group.find(params[:group_id])
     Vote.create(group_id: params[:group_id], user_id: current_user.id)
